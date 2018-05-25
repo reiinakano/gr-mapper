20294e8c0af32e4e77e9cf21af73ea992ccfbd21
Reiichiro Nakano <reiichiro@istellar.jp>
2018-05-25 13:17:23 +0900

    some plotting

--- python/prbs_sink_b.py
+++ python/prbs_sink_b.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
+import matplotlib
+matplotlib.use('agg')
 
 import numpy
 from gnuradio import gr
 import prbs_base
+import matplotlib.pyplot as plt
 
 class prbs_sink_b(gr.sync_block):
     def __init__(self, which_mode="PRBS31", reset_len=100000, skip=100000):
@@ -14,16 +17,29 @@ class prbs_sink_b(gr.sync_block):
         self.nbits = 0.0
         self.nerrs = 0.0
         self.skip  = skip
+        self.array_nbits = []
+        self.array_ber = []
 
     def work(self, input_items, output_items):
         inb = input_items[0]
         gen = self.base.gen_n(len(inb))
-        if(self.nitems_read(0) > self.skip):
+        if self.nitems_read(0) > self.skip:
             # only count bit errors after first skip bits
             self.nerrs += numpy.sum(numpy.bitwise_xor(inb, gen).astype('float32'))
             self.nbits += len(inb)
         if self.nbits > 0:
             print "NBits: %d \tNErrs: %d \tBER: %.4E"%(int(self.nbits), int(self.nerrs), self.nerrs/self.nbits)
-            #print "NBits: %d \tNErrs: %d \tBER: %g"%(int(self.nbits), int(self.nerrs), self.nerrs/self.nbits)
+            self.array_nbits.append(int(self.nbits))
+            self.array_ber.append(self.nerrs/self.nbits)
         return len(inb)
 
+    def stop(self):
+        plt.plot(self.array_nbits, self.array_ber)
+        plt.plot(self.array_nbits, numpy.full([len(self.array_ber)], self.array_ber[-1]),
+                 linestyle=':', label='average BER = {}'.format(self.array_ber[-1]))
+        plt.xlabel('Number of bits sent')
+        plt.ylabel('BER')
+        plt.title('BER vs. Number of bits sent ()')
+        plt.legend()
+        plt.savefig('ber.png')
+        return True
